59e1f8d92c733b6420151a1bf6874b33b3d3150a
lenotle <skyhellode@163.com>
2022-06-14 15:59:17 +0800

    fix2

--- main.tf
+++ main.tf
@@ -1,14 +1,17 @@
 terraform {
-    required_providers {
-      alicloud = {
-        source = "aliyun/alicloud"
-        version = "1.99.0"
-      }
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = "1.99.0"
     }
-}
-  provider "alicloud" {
-    # Configuration options
   }
+}
+provider "alicloud" {
+  # Configuration options
+  access_key = "LTAI5tLx8a7JJQU2UBwNwDtm"
+  secret_key = "onYrXy31oxfD6ViYupskDdaHhFT22c"
+  region = "cn-shenzhen"
+}
 
 resource "alicloud_vpc" "vpc" {
   name       = "tf_test_foo"
@@ -22,20 +25,20 @@ resource "alicloud_vswitch" "vsw" {
 }
 
 resource "alicloud_security_group" "default" {
-  name = "default"
+  name   = "default"
   vpc_id = alicloud_vpc.vpc.id
 }
 
 resource "alicloud_instance" "instance" {
   # cn-shenzhen
   availability_zone = "cn-shenzhen-b"
-  security_groups = alicloud_security_group.default.*.id
+  security_groups   = alicloud_security_group.default.*.id
   # series III
-  instance_type        = "ecs.n2.small"
-  system_disk_category = "cloud_efficiency"
-  image_id             = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
-  instance_name        = "test_foo"
-  vswitch_id = alicloud_vswitch.vsw.id
+  instance_type              = "ecs.n2.small"
+  system_disk_category       = "cloud_efficiency"
+  image_id                   = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
+  instance_name              = "test_foo"
+  vswitch_id                 = alicloud_vswitch.vsw.id
   internet_max_bandwidth_out = 10
 }
 
