342fa594c77446c9f4719e3208e8cc0ebd9af2a1
lenotle <skyhellode@163.com>
2022-06-14 15:16:40 +0800

    add alicloud ecs

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+    required_providers {
+      alicloud = {
+        source = "aliyun/alicloud"
+        version = "1.99.0"
+      }
+    }
+}
+  provider "alicloud" {
+    # Configuration options
+  }
+
+resource "alicloud_vpc" "vpc" {
+  name       = "tf_test_foo"
+  cidr_block = "172.16.0.0/12"
+}
+
+resource "alicloud_vswitch" "vsw" {
+  vpc_id            = alicloud_vpc.vpc.id
+  cidr_block        = "172.16.0.0/21"
+  availability_zone = "cn-shenzhen-b"
+}
+
+resource "alicloud_security_group" "default" {
+  name = "default"
+  vpc_id = alicloud_vpc.vpc.id
+}
+
+resource "alicloud_instance" "instance" {
+  # cn-shenzhen
+  availability_zone = "cn-shenzhen-b"
+  security_groups = alicloud_security_group.default.*.id
+  # series III
+  instance_type        = "ecs.n2.small"
+  system_disk_category = "cloud_efficiency"
+  image_id             = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
+  instance_name        = "test_foo"
+  vswitch_id = alicloud_vswitch.vsw.id
+  internet_max_bandwidth_out = 10
+}
+
+resource "alicloud_security_group_rule" "allow_all_tcp" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "1/65535"
+  priority          = 1
+  security_group_id = alicloud_security_group.default.id
+  cidr_ip           = "0.0.0.0/0"
+}
